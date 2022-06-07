e045e7e5c7fba5b3de26bbd3121c1d87aa120e74
Piyush Raja <piyush1.bksc@gmail.com>
2022-06-07 13:15:09 +0530

    Django continued

--- projects/views.py
+++ projects/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request) :
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html')
 
 def project(request,pk) :
-    return HttpResponse('SINGLE PROJECT' + " " +str(pk))
+    # return HttpResponse('SINGLE PROJECT' + " " +str(pk))
+    return render(request,'projects/single-project.html' )
 
