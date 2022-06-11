23fb861be7e65f867a9fcc13138f8b0652f0d227
piyushrj100 <piyush1.bksc@gmail.com>
2022-06-12 02:30:17 +0530

    theme improved

--- django_app/settings.py
+++ django_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^bb51!v617!791l+-%4zqs$gjt5bv8#gd*65chvsz29^^5#ek9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
