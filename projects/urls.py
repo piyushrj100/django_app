a09a0b585881ba74105e79b75f9abc45b9fdef11
piyushrj100 <piyush1.bksc@gmail.com>
2022-06-13 18:34:02 +0530

    users added

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ urlpatterns =[
      path('projects/', views.projects,name="projects"),
     path('project/<str:pk>/',views.project,name="project"),
     path('create-project/',views.createProject,name='create-project'),
-    path('', views.projects,name="projects"),
+    # path('', views.projects,name="projects"),
     path('update-project/<str:pk>/',views.updateProject,name="update-project"),
     path('delete-project/<str:pk>/',views.deleteProject,name="delete-project"),
 
