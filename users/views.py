c0c7c36b2a600494cb6ade79cec5b516b8b5c0ee
piyushrj100 <piyush1.bksc@gmail.com>
2022-06-15 02:42:57 +0530

    profile added

--- users/views.py
+++ users/views.py
@@ -8,3 +8,10 @@ def profiles(request) :
     profiles=Profile.objects.all()
     context={'profiles':profiles}
     return render(request,'users/profiles.html',context)
+
+def userProfile(request,pk) :
+    profile=Profile.objects.get(id=pk)
+    topskills = profile.skill_set.exclude(description__exact="") #accessing the child model skill and filter all skills that is not empty 
+    otherskills=profile.skill_set.filter(description="")
+    context={'profile':profile,'topskills':topskills,'otherskills':otherskills}
+    return render(request,'users/user-profile.html',context)
