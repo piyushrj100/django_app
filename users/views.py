7f5543fbf82f37453fe820bf210faac12a1f5e71
piyushrj100 <piyush1.bksc@gmail.com>
2022-06-14 18:08:25 +0530

    continued

--- users/views.py
+++ users/views.py
@@ -1,4 +1,10 @@
+# Create your views here.
 from django.shortcuts import render
+from .models import Profile
 def profiles(request) :
     return render(request, "users/profiles.html")
-# Create your views here.
+
+def profiles(request) :
+    profiles=Profile.objects.all()
+    context={'profiles':profiles}
+    return render(request,'users/profiles.html',context)
