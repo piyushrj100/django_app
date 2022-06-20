fd53d4fbb7819215db57d59ce4d1d5cad90b7860
piyushrj100 <piyush1.bksc@gmail.com>
2022-06-20 23:43:52 +0530

    account edit added

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class CustomUserCreationForm(UserCreationForm) :
     class Meta:
@@ -15,3 +16,10 @@ class CustomUserCreationForm(UserCreationForm) :
             # self.fields['title'].widget.attr.update({'class':'input','placeholder':'Add title'})
             for name,field in self.fields.items():
                 field.widget.attrs.update({'class':'input'})
+
+class ProfileForm(ModelForm) :
+    class Meta :
+        model=Profile
+        # fields="__all__"
+        fields=['name','email','username','bio','short_intro','profile_image','social_github','social_twitter',
+        'social_linkedin','social_youtube','social_website']
--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomUserCreationForm   
+from .forms import CustomUserCreationForm, ProfileForm
 
 #Usercreation form for creating new users 
 def loginUser(request) :
@@ -73,5 +73,17 @@ def userAccount(request) :
 
     context={'profile':profile,'skills':skills,'projects':projects}
     return render(request,'users/account.html', context)
+
+@login_required(login_url='login')
+def editAccount(request) :
+    profile=request.user.profile #logged in user
+    form=ProfileForm(instance=profile)
+    if request.method=="POST" :
+        form = ProfileForm(request.POST,request.FILES,instance=profile)
+        if form.is_valid() :
+            form.save()
+            return  redirect('account')
+    context={'form':form}
+    return render(request, 'users/profile_form.html', context)
 
 
