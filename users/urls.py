b692df0a70c4e8994d873a7bdd9075357d7ac1b3
piyushrj100 <piyush1.bksc@gmail.com>
2022-06-21 18:12:55 +0530

    skill edit added

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('register/',views.registerUser,name="register"),
     path('account/', views.userAccount,name="account"),
     path('edit-account/', views.editAccount, name="edit-account"),
+    path('create-skill/',views.createSkill,name='create-skill'),
+    path('update-skill/<str:pk>/' ,views.updateSkill,name='update-skill'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import CustomUserCreationForm, ProfileForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 
 #Usercreation form for creating new users 
 def loginUser(request) :
@@ -85,5 +85,35 @@ def editAccount(request) :
             return  redirect('account')
     context={'form':form}
     return render(request, 'users/profile_form.html', context)
+
+@login_required(login_url='login')
+def createSkill(request) :
+    profile=request.user.profile
+    form=SkillForm()
+    if request.method=="POST" :
+        form=SkillForm(request.POST)
+        if form.is_valid() :
+            skill=form.save(commit=False) 
+            skill.owner=profile
+            skill.save()
+            return redirect('account')
+    context={'form' :form}
+    return render(request, 'users/skill_form.html',context)
+
+
+@login_required(login_url='login')
+def updateSkill(request, pk) :
+    profile=request.user.profile
+    skill=profile.skill_set.get(id=pk)
+    form=SkillForm(instance=skill)
+    if request.method=="POST" :
+        form=SkillForm(request.POST,instance=skill)
+        if form.is_valid() :
+            skill=form.save(commit=False) 
+            skill.owner=profile
+            skill.save()
+            return redirect('account')
+    context={'form' :form}
+    return render(request, 'users/skill_form.html',context)
 
 
