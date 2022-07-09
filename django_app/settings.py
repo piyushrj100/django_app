b78028f52bf85cffb8ce4b85ff96d3326db6d88f
piyushrj100 <piyush1.bksc@gmail.com>
2022-07-10 01:03:54 +0530

    allowed hosts corrected

--- django_app/settings.py
+++ django_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','piyush-jango-app.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','piyush-django-app.herokuapp.com']
 
 
 # Application definition
